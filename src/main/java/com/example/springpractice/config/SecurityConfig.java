8969328f9e61480c16c202ff1e131a3d9a85bfb6
Hyeonseop-Noh <heonsub6558@gmail.com>
2022-03-31 11:45:33 +0900

    add: Set the SecurityConfig

--- src/main/java/com/example/springpractice/config/SecurityConfig.java
+++ src/main/java/com/example/springpractice/config/SecurityConfig.java
@@ -1,4 +1,66 @@
 package com.example.springpractice.config;
 
-public class SecurityConfig {
+
+import com.example.springpractice.service.MemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private MemberService memberService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // Ignore the certification of files in static directory (= always pass)
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // page authority setting
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/myinfo").hasRole("MEMBER")
+                .antMatchers("/**").permitAll()
+                .and()
+
+                // login setting
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/user/logout/result").permitAll()
+                .and()
+
+                // logout setting
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                .logoutSuccessUrl("/user/logout/result")
+                .invalidateHttpSession(true)
+                .and()
+
+                // 403 exception setting
+                .exceptionHandling().accessDeniedPage("/user/denied");
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+    }
+
 }
